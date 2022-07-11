8f65d399602f9ef29c239f63f1798d9a9f341319
Konijima <mathieugermain1991@gmail.com>
2022-07-11 08:51:43 -0400

    Update Config.lua

--- Client-Server-Concept/lua/shared/MyModName/Config.lua
+++ Client-Server-Concept/lua/shared/MyModName/Config.lua
@@ -20,44 +20,16 @@ local Config = {
 
     --- Define custom events to add on the client
     ClientEvents = {
-
         --- Triggered when the mod Global ModData is done initializing
         --- [ isNewGame:boolean ]
         "OnModDataInitialized",
-
-        --- Triggered when a player has spawned and re-spawned
-        --- [ player:IsoPlayer, isNewCharacter:boolean ]
-        "OnPlayerSpawned",
-
-        --- Triggered when a player has died
-        --- [ player:IsoPlayer ]
-        "OnPlayerDied",
-
     },
 
     --- Define custom events to add on the server
     ServerEvents = {
-
         --- Triggered when the mod Global ModData is done initializing
         --- [ isNewGame:boolean ]
         "OnModDataInitialized",
-
-        --- Triggered when a player has connected and exist in the world
-        --- [ player:IsoPlayer, isNewPlayer:boolean ]
-        "OnPlayerConnected",
-
-        --- Triggered when a player has disconnected
-        --- [ player:IsoPlayer ]
-        "OnPlayerDisconnected",
-
-        --- Triggered when a player has spawned and re-spawned
-        --- [ player:IsoPlayer, isNewCharacter:boolean ]
-        "OnPlayerSpawned",
-
-        --- Triggered when a player has died
-        --- [ player:IsoPlayer ]
-        "OnPlayerDied",
-
     },
 
 };
