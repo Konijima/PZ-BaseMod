01feee5caee545d02b22e20cde6cdc5311158ae1
Konijima <mathieugermain1991@gmail.com>
2022-07-11 07:15:20 -0400

    Add client and server events table into config

--- Client-Server-Concept/lua/shared/MyModName/Config.lua
+++ Client-Server-Concept/lua/shared/MyModName/Config.lua
@@ -4,6 +4,16 @@ local Config = {
     ModName = "MyModName",
     ModVersion = "1.0.0",
 
+    --- Define which custom event to add in the client
+    ClientEvents = {
+
+    },
+
+    --- Define which custom event to add in the server
+    ServerEvents = {
+        
+    },
+
 };
 
 print("Initializing " .. Config.ModName .. " version " .. Config.ModVersion);
