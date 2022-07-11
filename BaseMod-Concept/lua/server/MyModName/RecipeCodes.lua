dae0ccdb9ea22641c83806337de73b5320539bef
Konijima <mathieugermain1991@gmail.com>
2022-07-11 10:04:42 -0400

    Update RecipeCodes.lua

--- BaseMod-Concept/lua/server/MyModName/RecipeCodes.lua
+++ BaseMod-Concept/lua/server/MyModName/RecipeCodes.lua
@@ -9,29 +9,16 @@
 ---------------------------------------------------------------------------------
 
 --- Preparing the tables
-local t = MyModName_RecipeCodes or {};
-t.GetItemTypes = t.GetItemTypes or {};
-t.OnCanPerform = t.OnCanPerform or {};
-t.OnCreate = t.OnCreate or {};
-t.OnGiveXP = t.OnGiveXP or {};
-t.OnTest = t.OnTest or {};
-MyModName_RecipeCodes = t;
+MyModName_RecipeCodes = {
+    GetItemTypes = {},
+    OnCanPerform = {},
+    OnCreate = {},
+    OnGiveXP = {},
+    OnTest = {},
+}
 
 ---------------------------------------------------------------------------------
 
 --- Define your recipe functions here
 
---- Example:
-function MyModName_RecipeCodes.OnCreate.MakeBaseballBat(items, result, playerObj)
-    playerObj:getInventory():AddItem(result:getFullType()); --- add one more
-end
 
---[[ Usage in recipe txt script:
-
-    recipe Make Baseball Bat {
-        ...
-        OnCreate:MyModName_RecipeCodes.OnCreate.MakeBaseballBat
-        ...
-    }
-    
-]]
