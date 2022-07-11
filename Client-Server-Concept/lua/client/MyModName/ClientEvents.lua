6213ac8c97dc42333e9b7615d2330790c227c8a2
Konijima <mathieugermain1991@gmail.com>
2022-07-11 07:36:55 -0400

    Update ClientEvents.lua

--- Client-Server-Concept/lua/client/MyModName/ClientEvents.lua
+++ Client-Server-Concept/lua/client/MyModName/ClientEvents.lua
@@ -1,6 +1,35 @@
 --- Load Client
 local Client = require 'MyModName/Client';
 
+local addedEvents = ArrayList.new();
+
 for _, eventName in ipairs(Client.Config.ClientEvents) do
-    LuaEventManager.AddEvent(eventName);
+    if not addedEvents:contains(eventName) then
+        LuaEventManager.AddEvent(Client.Config.ModName .. "Client" .. eventName);
+        addedEvents:add(eventName);
+    end
+end
+
+---@param eventName string
+---@param callback function
+function Client.AddEvent(eventName, callback)
+    if addedEvents:contains(eventName) then
+        Events[Client.Config.ModName .. "Client" .. eventName].Add(callback);
+    end
+end
+
+---@param eventName string
+---@param callback function
+function Client.RemoveEvent(eventName, callback)
+    if addedEvents:contains(eventName) then
+        Events[Client.Config.ModName .. "Client" .. eventName].Remove(callback);
+    end
+end
+
+---@param eventName string
+---@vararg arguments
+function Client.TriggerEvent(eventName, ...)
+    if addedEvents:contains(eventName) then
+        triggerEvent(Client.Config.ModName .. "Client" .. eventName, ...);
+    end
 end
