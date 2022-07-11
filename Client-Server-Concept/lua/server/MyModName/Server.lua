ae0608e8c53e49944459a1234612037ddc2ab975
Konijima <mathieugermain1991@gmail.com>
2022-07-11 08:17:14 -0400

    Refactor Server to ModServer

--- Client-Server-Concept/lua/server/MyModName/Server.lua
+++ Client-Server-Concept/lua/server/MyModName/Server.lua
@@ -4,8 +4,8 @@ local Config = require "MyModName/Config";
 --- Load Utils
 local Utils = require "MyModName/Utils";
 
----@class Server
-local Server = {
+---@class ModServer
+local ModServer = {
     Config = Config,    -- Reference to the mod Config class
     Utils = Utils,      -- Reference to the mod Utils class
     Data = {},          -- Global ModData will be stored in this
@@ -15,22 +15,22 @@ local Server = {
 
 ---Log data prefixed with the mod name
 ---@param str string
-function Server.Log(str)
-    print(Server.Config.ModName .. ": " .. tostring(str));
+function ModServer.Log(str)
+    print(ModServer.Config.ModName .. ": " .. tostring(str));
 end
 
 ---Send data to one or all clients
 ---@param command string
 ---@param data table
 ---@param _player IsoPlayer
-function Server.SendCommand(command, data, _player)
+function ModServer.SendCommand(command, data, _player)
     if not data then data = {}; end 
     if type(data) ~= "table" then error("Invalid 'data' parameter type, can only send 'nil' or 'table' to client(s)."); end
 
     if instanceof(_player, "IsoPlayer") then
-        sendServerCommand(_player, Server.Config.ModName, command, data);
+        sendServerCommand(_player, ModServer.Config.ModName, command, data);
     elseif _player == nil then
-        sendServerCommand(Server.Config.ModName, command, data);
+        sendServerCommand(ModServer.Config.ModName, command, data);
     else
         error("Invalid '_player' parameter type.")
     end
@@ -38,12 +38,12 @@ end
 
 --- Handle received client commands
 local function receiveServerCommand(module, command, player, args)
-    if module ~= Server.Config.ModName then return; end
+    if module ~= ModServer.Config.ModName then return; end
 
-    if type(Server.Commands[command]) == "function" then
-        Server.Commands[command](player, args);
+    if type(ModServer.Commands[command]) == "function" then
+        ModServer.Commands[command](player, args);
     end
 end
 Events.OnServerCommand.Add(receiveServerCommand);
 
-return Server;
+return ModServer;
